c0876eb9b8870f987ed84c417da5e5b3835c92ed
Owen DiBacco <OwenDiBacco04@gmail.com>
2024-12-17 13:54:49 -0500

    remove expirimenting module

--- Testing_Things.py
+++
@@ -1,6 +0,0 @@
-# chunking the array to control the concurrency 
-playlists = np.array(playlists) # creates a numpy array
-number_of_arrays = len(playlists) / 5 # replace denominator with the number of index you want per array -1
-array_of_playlists = np.array_split(playlists, number_of_arrays)
-for playlist in array_of_playlists:
-    print(playlist)
